2967d586a90f69a2f614ba8800bdb80f55118292
korzhenevskaya <kristinkor@gmail.com>
2025-05-12 19:28:45 -0400

    ignore map

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,16 +1,7 @@
-// pages/index.tsx
-// import dynamic from 'next/dynamic'
 import HeroSection from '../components/HeroSection'
 import Announcements from '../components/Announcements'
 import AboutNaclo from '../components/AboutNaclo'
 import DonateContact from '../components/DonateContact'
-// import SiteMapWrapper from '@/components/SiteMapWrapper'
-
-// // Dynamically import SiteMap for client-side rendering only
-// const DynamicSiteMap = dynamic(() => import('../components/SiteMap'), {
-//   ssr: false,
-//   loading: () => <p>Loading map...</p>,
-// })
 
 export default function Home() {
   return (
@@ -19,7 +10,6 @@ export default function Home() {
       <AboutNaclo />
       <Announcements />
       <DonateContact />
-      {/* <SiteMapWrapper /> */}
     </div>
   )
 }
