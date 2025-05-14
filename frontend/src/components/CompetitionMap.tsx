c63e4cdc775dafc82bc21b0893a163585f05f2a5
korzhenevskaya <kristinkor@gmail.com>
2025-05-14 02:32:25 -0400

    map fix

--- frontend/src/components/CompetitionMap.tsx
+++ frontend/src/components/CompetitionMap.tsx
@@ -1,36 +1,19 @@
-// src/components/CompetitionMap.tsx
 'use client'
 
-import dynamic from 'next/dynamic'
-import { useEffect, useState } from 'react'
-import L from 'leaflet'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import L, { LatLngExpression } from 'leaflet'
 
-const MapContainer = dynamic(
-  () => import('react-leaflet').then((mod) => mod.MapContainer),
-  { ssr: false }
-)
-const TileLayer = dynamic(
-  () => import('react-leaflet').then((mod) => mod.TileLayer),
-  { ssr: false }
-)
-const Marker = dynamic(
-  () => import('react-leaflet').then((mod) => mod.Marker),
-  { ssr: false }
-)
-const Popup = dynamic(() => import('react-leaflet').then((mod) => mod.Popup), {
-  ssr: false,
+// Fix Leaflet marker icons for SSR-compatible environments
+Object.assign(L.Icon.Default.prototype, {
+  _getIconUrl: () => '',
 })
 
-// Fix leaflet marker icons on client side only
-if (typeof window !== 'undefined') {
-  delete (L.Icon.Default.prototype as any)._getIconUrl
-  L.Icon.Default.mergeOptions({
-    iconRetinaUrl:
-      'https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon-2x.png',
-    iconUrl: 'https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon.png',
-    shadowUrl: 'https://unpkg.com/leaflet@1.9.3/dist/images/marker-shadow.png',
-  })
-}
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    'https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.3/dist/images/marker-shadow.png',
+})
 
 interface Site {
   id: number
@@ -52,34 +35,33 @@ const sites: Site[] = [
     longitude: -71.2619,
     website: 'http://boston-olympiad.org',
   },
-  // ... другие сайты можно сюда добавить
+  {
+    id: 2,
+    name: 'Carnegie Mellon University',
+    city: 'Pittsburgh',
+    state: 'PA',
+    latitude: 40.4433,
+    longitude: -79.9436,
+    website: 'https://www.lti.cs.cmu.edu/naclo-cmu',
+  },
 ]
 
 export default function CompetitionMap() {
-  const [isClient, setIsClient] = useState(false)
-
-  useEffect(() => {
-    setIsClient(true)
-  }, [])
-
-  if (!isClient) return null
+  const center: LatLngExpression = [39.8283, -98.5795]
 
   return (
     <MapContainer
-      center={[39.8283, -98.5795]}
+      center={center}
       zoom={4}
       scrollWheelZoom={false}
-      style={{ height: '500px', width: '100%' }}
+      style={{ height: '600px', width: '100%' }}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       {sites.map((site) => (
-        <Marker
-          key={site.id}
-          position={[site.latitude, site.longitude] as [number, number]}
-        >
+        <Marker key={site.id} position={[site.latitude, site.longitude]}>
           <Popup>
             <strong>{site.name}</strong>
             <br />
