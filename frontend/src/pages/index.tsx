faa5c579095f849141b7673f1b5cb25c1f8bb582
korzhenevskaya <kristinkor@gmail.com>
2025-05-14 02:34:16 -0400

    delete map

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,13 +3,6 @@ import Announcements from '../components/Announcements'
 import AboutNaclo from '../components/AboutNaclo'
 import SponsorsCarousel from '../components/SponsorsCarousel'
 import DonateContact from '../components/DonateContact'
-import dynamic from 'next/dynamic'
-
-const CompetitionMap = dynamic(() => import('../components/CompetitionMap'), {
-  ssr: false,
-})
-// ... inside your page/component JSX:
-;<CompetitionMap />
 
 export default function Home() {
   return (
@@ -19,7 +12,6 @@ export default function Home() {
       <Announcements />
       <DonateContact />
 
-      <CompetitionMap />
       <SponsorsCarousel />
     </div>
   )
